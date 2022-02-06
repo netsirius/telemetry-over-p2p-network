88ab5d37282de43111f1fe775d7b9355b8d82d10
Hector Santos <netsirius@gmail.com>
2022-02-06 23:06:36 +0100

    fix sample and crates

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,16 @@ use libp2p::{
 };
 use log::{error, info};
 use once_cell::sync::Lazy;
-use opentelemetry::trace::TraceError;
-use opentelemetry::{
-    global,
-    sdk::trace as sdktrace,
-    trace::{FutureExt, TraceContextExt, Tracer},
-    Context,
-};
+// use opentelemetry::trace::TraceError;
+// use opentelemetry::{
+//     global,
+//     sdk::trace as sdktrace,
+//     trace::{FutureExt, TraceContextExt, Tracer},
+//     Context,
+// };
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
+use std::error::Error;
 use tokio::{fs, io::AsyncBufReadExt, sync::mpsc};
 
 const STORAGE_FILE_PATH: &str = "./documents.json";
@@ -66,6 +67,7 @@ enum EventType {
 /// This macro implements all the NetworkBehaviour trait's functions for all the members of
 /// the struct, which are not annotated with behaviour(ignore)
 #[derive(NetworkBehaviour)]
+#[behaviour(event_process = true)]
 struct DocumentBehaviour {
     floodsub: Floodsub,
     mdns: Mdns,
@@ -153,36 +155,36 @@ impl NetworkBehaviourEventProcess<MdnsEvent> for DocumentBehaviour {
     }
 }
 
-fn init_tracer() -> Result<opentelemetry::sdk::trace::Tracer, TraceError> {
-    opentelemetry_jaeger::new_pipeline()
-        .with_service_name("trace-demo")
-        .install_batch(opentelemetry::runtime::Tokio)
-}
+// fn init_tracer() -> Result<opentelemetry::sdk::trace::Tracer, TraceError> {
+//     opentelemetry_jaeger::new_pipeline()
+//         .with_service_name("trace-demo")
+//         .install_batch(opentelemetry::runtime::Tokio)
+// }
 
-async fn create_new_doc(name: &str, author: &str, content: &str) -> Result<(), Err> {
+async fn create_new_doc(name: &str, author: &str, content: &str) -> Result<(), anyhow::Error> {
     let mut local_docs = read_local_docs().await?;
     let new_id = match local_docs.iter().max_by_key(|r| r.id) {
         Some(v) => v.id + 1,
         None => 0,
     };
-    local_docs.push(Recipe {
+    local_docs.push(Document {
         id: new_id,
         name: name.to_owned(),
-        ingredients: ingredients.to_owned(),
-        instructions: instructions.to_owned(),
+        author: author.to_owned(),
+        content: content.to_owned(),
         public: false,
     });
     write_local_docs(&local_docs).await?;
 
-    info!("Created recipe:");
+    info!("Created document:");
     info!("Name: {}", name);
-    info!("Ingredients: {}", ingredients);
-    info!("Instructions:: {}", instructions);
+    info!("Author: {}", author);
+    info!("Content: {}", content);
 
     Ok(())
 }
 
-async fn publish_doc(id: usize) -> Result<(), Err> {
+async fn publish_doc(id: usize) -> Result<(), anyhow::Error> {
     let mut local_docs = read_local_docs().await?;
     local_docs
         .iter_mut()
@@ -192,66 +194,68 @@ async fn publish_doc(id: usize) -> Result<(), Err> {
     Ok(())
 }
 
-async fn read_local_docs() -> Result<Documents, Err> {
+async fn read_local_docs() -> Result<Documents, anyhow::Error> {
     let content = fs::read(STORAGE_FILE_PATH).await?;
     let result = serde_json::from_slice(&content)?;
     Ok(result)
 }
 
-async fn write_local_docs(docs: &Documents) -> Result<(), Err> {
+async fn write_local_docs(docs: &Documents) -> Result<(), anyhow::Error> {
     let json = serde_json::to_string(&docs)?;
     fs::write(STORAGE_FILE_PATH, &json).await?;
     Ok(())
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>>{
     let peer_id = PEER_ID.clone();
     info!("Peer Id: {}", peer_id);
 
-    let tracer = init_tracer()?;
-    let span = tracer.start("node_" + peer_id);
-    let cx = Context::current_with_span(span);
+    // let tracer = init_tracer()?;
+    // let span = tracer.start(format!("node_{}", peer_id));
+    // let cx = Context::current_with_span(span);
 
-    /// Async channel to communicate between different parts of the application
+    // Async channel to communicate between different parts of the application
     let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
 
-    /// Some authentication keys, which we’ll use to secure the traffic within the network
+    // Some authentication keys, which we’ll use to secure the traffic within the network
     let auth_keys = Keypair::<X25519Spec>::new()
         .into_authentic(&KEYS)
         .expect("can't create auth keys");
 
-    /// Set TCP transport protocol that enables connection and communication between peers
+    // Set TCP transport protocol that enables connection and communication between peers
     let transp = TokioTcpConfig::new()
         .upgrade(upgrade::Version::V1)
         .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
         .multiplex(mplex::MplexConfig::new())
         .boxed();
 
-    /// Define the network behaviour (logic of the netwoek and all peers).
-    /// Using floodsub protocol to deal with network events
-    /// mDNS protocol for discovering other peers on the local network
-    /// response_sender to propagate events back to the main part of the application.
+    // Define the network behaviour (logic of the netwoek and all peers).
+    // Using floodsub protocol to deal with network events
+    // mDNS protocol for discovering other peers on the local network
+    // response_sender to propagate events back to the main part of the application.
     let mut behaviour = DocumentBehaviour {
         floodsub: Floodsub::new(PEER_ID.clone()),
-        mdns: TokioMdns::new().expect("can create mdns"),
+        mdns: Mdns::new(Default::default())
+            .await
+            .expect("can create mdns"),
         response_sender,
     };
 
-    /// Subscribe to the documents topic, now we can send and receive events on this topic
+    // Subscribe to the documents topic, now we can send and receive events on this topic
     behaviour.floodsub.subscribe(TOPIC.clone());
 
-    /// Manage the created tcp connections and executes the network behaviour, triggering and
-    /// receiving events and giving us a way to get to them from the outside.
-    /// Uses tokio runtime to run internally
-    /// Swarm contains the state og the network as a whole. (https://docs.rs/libp2p/latest/libp2p/swarm/index.html)
+    // Manage the created tcp connections and executes the network behaviour, triggering and
+    // receiving events and giving us a way to get to them from the outside.
+    // Uses tokio runtime to run internally
+    // Swarm contains the state og the network as a whole. (https://docs.rs/libp2p/latest/libp2p/swarm/index.html)
     let mut swarm = SwarmBuilder::new(transp, behaviour, PEER_ID.clone())
         .executor(Box::new(|fut| {
             tokio::spawn(fut);
         }))
         .build();
 
-    /// Start the swarm
+    // Start the swarm
     Swarm::listen_on(
         &mut swarm,
         "/ip4/0.0.0.0/tcp/0"
@@ -262,24 +266,24 @@ async fn main() {
 
     let mut stdin = tokio::io::BufReader::new(tokio::io::stdin()).lines();
 
-    /// Event loop
+    // Event loop
     loop {
         let evt = {
-            /// Wait for these three async processes, handling the first one that finishes
+            // Wait for these three async processes, handling the first one that finishes
             tokio::select! {
                 line = stdin.next_line() => Some(EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
-                event = swarm.next() => {
+                response = response_rcv.recv() => Some(EventType::Response(response.expect("response exists"))),
+                event = swarm.select_next_some() => {
                     info!("Unhandled Swarm Event: {:?}", event);
                     None
                 },
-                response = response_rcv.recv() => Some(EventType::Response(response.expect("response exists"))),
             }
         };
         if let Some(event) = evt {
             match event {
                 EventType::Response(resp) => {
                     let json = serde_json::to_string(&resp).expect("can jsonify response");
-                    swarm.floodsub.publish(TOPIC.clone(), json.as_bytes()); // publish the response
+                    swarm.behaviour_mut().floodsub.publish(TOPIC.clone(), json.as_bytes()); // publish the response
                 }
                 EventType::Input(line) => match line.as_str() {
                     "ls p" => handle_list_peers(&mut swarm).await,
@@ -293,12 +297,12 @@ async fn main() {
     }
 }
 
-/// HANDLERS FOR ALL OPERATIONS
+// HANDLERS FOR ALL OPERATIONS
 
 /// Discover and display all the network nodes
-async fn handle_list_peers(swarm: &mut Swarm<RecipeBehaviour>) {
+async fn handle_list_peers(swarm: &mut Swarm<DocumentBehaviour>) {
     info!("Discovered Peers:");
-    let nodes = swarm.mdns.discovered_nodes();
+    let nodes = swarm.behaviour().mdns.discovered_nodes();
     let mut unique_peers = HashSet::new();
     for peer in nodes {
         unique_peers.insert(peer);
@@ -306,7 +310,7 @@ async fn handle_list_peers(swarm: &mut Swarm<RecipeBehaviour>) {
     unique_peers.iter().for_each(|p| info!("{}", p));
 }
 
-async fn handle_list_docs(cmd: &str, swarm: &mut Swarm<RecipeBehaviour>) {
+async fn handle_list_docs(cmd: &str, swarm: &mut Swarm<DocumentBehaviour>) {
     let rest = cmd.strip_prefix("ls d ");
     match rest {
         Some("all") => {
@@ -314,14 +318,14 @@ async fn handle_list_docs(cmd: &str, swarm: &mut Swarm<RecipeBehaviour>) {
                 mode: ListMode::ALL,
             };
             let json = serde_json::to_string(&req).expect("can jsonify request");
-            swarm.floodsub.publish(TOPIC.clone(), json.as_bytes());
+            swarm.behaviour_mut().floodsub.publish(TOPIC.clone(), json.as_bytes());
         }
         Some(docs_peer_id) => {
             let req = ListRequest {
                 mode: ListMode::One(docs_peer_id.to_owned()),
             };
             let json = serde_json::to_string(&req).expect("can jsonify request");
-            swarm.floodsub.publish(TOPIC.clone(), json.as_bytes());
+            swarm.behaviour_mut().floodsub.publish(TOPIC.clone(), json.as_bytes());
         }
         None => {
             match read_local_docs().await {
